e1a0f3a5f68a194a52b503299daa8dacce64f677
Bojan Dimitrovski <bojan.d@privax.com>
2017-03-06 15:34:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# WeatherGIF
+A simple node.js app that works with webtask.io to show the current weather using GIFs.
+
+## Usage
+- End users: Just visit this link in your browser: https://wt-2ae92d94dc01d22c4a4ef289252be7b2-0.run.webtask.io/app
+- Developers:
+
+    - Install WT-CLI: https://github.com/auth0/wt-cli
+    - Get a Dark Sky account and API key: https://darksky.net/dev/account
+    - Get a Cloudinary account and API key: http://cloudinary.com/
+    - Configure secrets in the app accordingly, and when ready, build the app like this:
+        ```wt create app.js --secret darkSkyApiKey=<YOURDARKSKYAPIKEY> --secret cloudinarycloudName=<YOURCLOUDINARYCLOUDNAME> --secret cloudinaryAPIKey=<CLOUDINARYAPIKEY> --secret cloudinaryAPISecret=<CLOUDINARYAPISECRET>```
+    - Boom, weather.
+
+## Notes
+
+- Dark Sky API in its free version allows 1,000	free calls.
+- Not all weather cases or parameters have been predicted - fork the repo and make something awesome.
+
+## Credit
+
+- Forecast: https://www.npmjs.com/package/forecast
+- Cloudinary: https://www.npmjs.com/package/cloudinary
---
+++ app.js
@@ -0,0 +1,84 @@
+// Require forecast and cloudinary
+var Forecast = require('forecast');
+var cloudinary = require('cloudinary');
+
+module.exports = function(context, cb, res) {
+
+  // Configure secrets
+  var darkSkyAPIKey = context.data.darkSkyApiKey;
+  var cloudinaryCloudName = context.data.cloudinarycloudName;
+  var cloudinaryAPIKey = context.data.cloudinaryAPIKey;
+  var cloudinaryAPISecret = context.data.cloudinaryAPISecret;
+
+  // Configure forecast
+  var forecast = new Forecast({
+    service: 'darksky',
+    key: darkSkyAPIKey,
+    units: 'celcius',
+    cache: true,
+    ttl: {
+      minutes: 27,
+      seconds: 45
+    }
+  });
+
+  // Configure cloudinary
+  cloudinary.config({
+    cloud_name: cloudinaryCloudName,
+    api_key: cloudinaryAPIKey,
+    api_secret: cloudinaryAPISecret
+  });
+
+  // Some general settings
+  var image = '';
+  var background = '#117A8E';
+
+  res.writeHead(200, { 'Content-Type': 'text/html '});
+
+  // Retrieve weather information from coordinates (Belgrade, Serbia) and display gif depending on the forecast
+  forecast.get([44.7866, 20.4489], function(err, weather) {
+    if (err) cb(null, 'Error retrieving data.');
+
+    switch (weather.currently.summary) {
+      case 'Sun':
+        image = cloudinary.image('sunny_s8ivlz.gif');
+        break;
+
+      case 'Partly Cloudy':
+        image = cloudinary.image("partly-sunny_t0xstu.gif");
+        break;
+
+      case 'Rain':
+        image = cloudinary.image("rain_wv83kb.gif");
+        background = "#a186be";
+        break;
+
+      default:
+        image = cloudinary.image("dress-accordingly_zydz74.gif");
+        background = "#69BDEE";
+    }
+
+    // Render the template
+    res.end(
+      '<!DOCTYPE html>' +
+      '<html>' +
+        '<head>' +
+          '<style>' +
+            'body {' +
+              'background-color:' + background + ';' +
+            '}' +
+            'img {' +
+              'display: block;' +
+              'margin: 0 auto;' +
+            '}' +
+          '</style>' +
+        '</head>' +
+        '<body>' +
+          '<div>' +
+            image +
+          '</div>' +
+        '</body>' +
+      '</html>'
+    );
+  });
+}
