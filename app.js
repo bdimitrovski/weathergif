f5c8b8be373b80a87d26fcface26a88029d2d83e
Bojan Dimitrovski <bojan.d@privax.com>
2017-03-06 23:03:01 +0100

    Improvements yiss add typography, additional weather conditions, gifs

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ module.exports = (context, cb, res) => {
   // Some general settings
   var image = '';
   var background = '#117A8E';
+  var temperature = '';
+  var looksLike = '';
 
   res.writeHead(200, { 'Content-Type': 'text/html '});
 
@@ -39,23 +41,37 @@ module.exports = (context, cb, res) => {
   forecast.get([44.7866, 20.4489], (err, weather) => {
     if (err) cb(null, 'Error retrieving data.');
 
+    // Get temperature
+    var temperature = weather.currently.temperature;
+
     switch (weather.currently.summary) {
       case 'Sun':
         image = cloudinary.image('sunny_s8ivlz.gif');
+        looksLike = 'Sunny and clear.';
         break;
 
       case 'Partly Cloudy':
         image = cloudinary.image("partly-sunny_t0xstu.gif");
+        looksLike = 'Some clouds. Take an umbrella just in case.';
         break;
 
       case 'Rain':
         image = cloudinary.image("rain_wv83kb.gif");
+        looksLike = 'Oh noes! It is raining :(';
         background = "#a186be";
         break;
 
       default:
         image = cloudinary.image("dress-accordingly_zydz74.gif");
+        looksLike = 'Clear and dry, but dress accordingly.';
         background = "#69BDEE";
+    }
+
+    // Additional weather conditions
+    if (weather.currently.icon === 'partly-cloudy-night') {
+        image = cloudinary.image("partly-cloudy-night_pocr27.gif");
+        looksLike = 'Some clouds.'
+        background = "#030303";
     }
 
     // Render the template
@@ -63,20 +79,40 @@ module.exports = (context, cb, res) => {
       '<!DOCTYPE html>' +
       '<html>' +
         '<head>' +
+          '<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">' +
           '<style>' +
             'body {' +
+              'font-family:' + 'Open Sans' + ', sans-serif;' +
               'background-color:' + background + ';' +
             '}' +
-            'img {' +
+            'img, h2, p {' +
               'display: block;' +
               'margin: 0 auto;' +
+              'text-align: center' +
             '}' +
+            'h2 {' +
+              'font-size: 30px;' +
+              'font-weight: 700;' +
+              'color: #fff;' +
+            '}' +
+            'p {' +
+              'font-size: 16px;' +
+              'margin-top: 5px;' +
+              'color: #fff;' +
           '</style>' +
         '</head>' +
         '<body>' +
           '<div>' +
             image +
-          '</div>' +
+          '<h2>' +
+            looksLike +
+          '</h2>' +
+          '<p>' +
+            'Current temperature: ' +
+            temperature +
+            " &deg;C" +
+          '</p>' +
+        '</div>' +
         '</body>' +
       '</html>'
     );
